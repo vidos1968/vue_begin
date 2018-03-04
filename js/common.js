42264875f4faa894f3b4a345d44f63af31883323
vidos1968 <dima@vidoss.ru>
2018-03-04 13:49:59 +0300

    First commit

---
+++ js/common.js
@@ -0,0 +1,48 @@
+
+var app = new Vue({
+el:'#app2',
+data:{
+message:'Вы загрузили эту страницу сегодня в :' + new Date().toLocaleString()
+
+}
+
+
+})
+
+var app3 = new Vue({
+
+el:'#app3',
+data:{
+    seen:true
+}
+
+})
+
+var app4 = new Vue({
+el:'#app4',
+data:{
+    todos:[
+        {text:'Посадить дерево'},
+        {text:'Построить дом'},
+        {text:'Вырастить сына'}
+
+    ]
+}
+
+})
+
+
+var app5 = new Vue({
+el:'#app5',
+data:{
+    message:'Привет, Vue!'
+},
+methods:{
+reverseMessage:function(){
+    this.message = this.message.split('').reverse('').join('')
+}
+    
+}
+
+
+})
